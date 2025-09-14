47dcd5722df711d48d8faf8135c5f9ccaa5ad832
นายชัยบูรณ์ บัวศิริ <chaiboon.bua@edu.bangkok.go.th>
2025-09-14 08:20:58 +0000

    https://i.postimg.cc/7h6Kmpwt/image.png นี้ลิงก์รูปโลโก้ ช้วยเปลี่ยนให้ท

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,7 +5,7 @@ import { cn } from '@/lib/utils';
 export function Logo({ className }: { className?: string }) {
   return (
     <Image
-      src="https://lh3.googleusercontent.com/d/10qUwamnvsdgwLO3GoNpXVcsaJHVixY9p"
+      src="https://i.postimg.cc/7h6Kmpwt/image.png"
       alt="Thai Grade Vision Logo"
       width={40}
       height={40}
